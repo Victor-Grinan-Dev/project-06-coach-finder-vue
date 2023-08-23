5738f2057fc88656fdad8ed532aad39162a3a444
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-23 15:26:10 +0300

    initial commit

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import coachDetail from "./pages/coaches/CoachDetail.vue";
+import coachesList from "./pages/coaches/CoachesList.vue";
+import coachRegistration from "./pages/coaches/CoachRegistration.vue";
+import contactCoach from "./pages/requests/ContactCoach.vue";
+import requestsRecieved from "./pages/requests/RequestsRecieved.vue";
+import notFound from "./pages/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: coachesList },
+    {
+      path: "/coaches/:id",
+      component: coachDetail,
+      children: [{ path: "contact", component: contactCoach }],
+    },
+    { path: "/register", component: coachRegistration },
+    { path: "/requests", component: requestsRecieved },
+    { path: "/:notfound(.*)", component: notFound },
+  ],
+});
+
+export default router;
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  modules: {},
+  state() {},
+  mutations: {},
+  actions: {},
+  getters: {},
+});
+
+export default store;
